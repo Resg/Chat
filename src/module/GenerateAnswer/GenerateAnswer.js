05c981182060552c11f8253dcf4f8fa2f4a62785
MishokkohsiM <mixanasa@mail.ru>
2019-11-23 18:02:14 +0300

    chat

--- src/module/GenerateAnswer/GenerateAnswer.js
+++ src/module/GenerateAnswer/GenerateAnswer.js
@@ -2,10 +2,9 @@
 
 export const GenerateAnswer = (req) => {
   let result = '';
-  switch (req) {
-    case /[Пп]ривет/.test(req):
-      console.log('asdasdfqf');
-      result+= 'Привет\n';
+  if (/[Пп]ривет/.test(req)){
+    console.log('asdasdfqf');
+    result+= 'Привет\n';
   }
   return result;
-}
+};
