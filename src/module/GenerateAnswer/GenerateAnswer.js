26688c4a05932dc9cd9a687b6a21617a57208eac
MishokkohsiM <mixanasa@mail.ru>
2019-11-23 18:11:00 +0300

    chat

--- src/module/GenerateAnswer/GenerateAnswer.js
+++ src/module/GenerateAnswer/GenerateAnswer.js
@@ -4,7 +4,10 @@ export const GenerateAnswer = (req) => {
   let result = '';
   if (/[Пп]ривет/.test(req)){
     console.log('asdasdfqf');
-    result+= 'Привет';
+    result+= 'Привет\n';
+  }
+  if (/(правил[ао])/.test(req.toLowerCase())) {
+    result+= 'Правила игры: http://93.171.139.195:780/rules\n';
   }
   return result;
 };
