3da016c2709c2a167dd687f631fdf7c52e6b257a
MishokkohsiM <mixanasa@mail.ru>
2019-11-23 14:47:20 +0300

    chat

--- src/main.js
+++ src/main.js
@@ -2,32 +2,32 @@ import User from "./module/User/UserContainer";
 
 
 window.user = new User();
-user.checkAuth();
-
-
-const socket = new WebSocket(`ws://93.171.139.196:781/chatRoom/?name=${user.username}`);
-const app = document.getElementById('chat');
-const textWindow = document.createElement('textarea');
-textWindow.className = 'chat__textWindow';
-textWindow.id = 'textWindow';
-socket.onmessage = (msg) => {
-  const data = JSON.parse(msg.data);
-  textWindow.textContent += `${data.author}: ${data.body}\n`;
-};
-app.appendChild(textWindow);
-const inputContainer = document.createElement('div');
-inputContainer.className = 'chat__inputContainer';
-const input = document.createElement('input');
-input.className = 'chat__inputContainer__input';
-const submitButton = document.createElement('button');
-submitButton.className = 'chat__inputContainer__button';
-submitButton.textContent = 'Отправить';
-submitButton.type = 'submit';
-inputContainer.appendChild(input);
-inputContainer.appendChild(submitButton);
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  socket.send(input.value);
-  input.value = '';
+user.checkAuth().then(()=>{
+  const socket = new WebSocket(`ws://93.171.139.196:781/chatRoom/?name=${user.username}`);
+  const app = document.getElementById('chat');
+  const textWindow = document.createElement('textarea');
+  textWindow.className = 'chat__textWindow';
+  textWindow.id = 'textWindow';
+  socket.onmessage = (msg) => {
+    const data = JSON.parse(msg.data);
+    textWindow.textContent += `${data.author}: ${data.body}\n`;
+  };
+  app.appendChild(textWindow);
+  const inputContainer = document.createElement('div');
+  inputContainer.className = 'chat__inputContainer';
+  const input = document.createElement('input');
+  input.className = 'chat__inputContainer__input';
+  const submitButton = document.createElement('button');
+  submitButton.className = 'chat__inputContainer__button';
+  submitButton.textContent = 'Отправить';
+  submitButton.type = 'submit';
+  inputContainer.appendChild(input);
+  inputContainer.appendChild(submitButton);
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    socket.send(input.value);
+    input.value = '';
+  });
+  app.appendChild(inputContainer);
 });
-app.appendChild(inputContainer);
+
