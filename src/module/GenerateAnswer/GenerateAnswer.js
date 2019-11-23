b54336189ae6f7864085e61da5cd8aace50eaec2
MishokkohsiM <mixanasa@mail.ru>
2019-11-23 18:21:53 +0300

    chat

--- src/module/GenerateAnswer/GenerateAnswer.js
+++ src/module/GenerateAnswer/GenerateAnswer.js
@@ -6,8 +6,17 @@ export const GenerateAnswer = (req) => {
     console.log('asdasdfqf');
     result+= 'Привет\n';
   }
-  if (/(правил[ао])/.test(req.toLowerCase())) {
+  if (/правил[ао]/.test(req.toLowerCase())) {
     result+= 'Правила игры: http://93.171.139.195:780/rules\n';
+  }
+  if (/игр/.test(req.toLowerCase())){
+    result+= 'Игра онлайн: http://93.171.139.195:780/online\n';
+  }
+  if (/интернет/.test(req.toLowerCase())){
+    result+= 'Игра без интернета: http://93.171.139.195:780/offline\n';
+  }
+  if (/изменить/.test(req.toLowerCase())){
+    result+= 'Настройка профиля доступна после авторизации по ссылке:\n http://93.171.139.195:780/profileChange\n'
   }
   return result;
 };
